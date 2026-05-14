0cdd099b6638c57f16b5d227ce580216aed04a4d
Barath Krishna Kumar <barath@lemonpay.tech>
2026-05-14 12:42:48 +0530

    fix: handle missing MONGODB_URI at build time

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,22 +1,28 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-
-if (!uri) {
-    throw new Error('Missing MONGODB_URI environment variable.');
-}
+const uri = process.env.MONGODB_URI as string;
 
 declare global {
     // eslint-disable-next-line no-var
     var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-const client = new MongoClient(uri);
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
-const clientPromise = global._mongoClientPromise || client.connect();
-
-if (process.env.NODE_ENV !== 'production') {
-    global._mongoClientPromise = clientPromise;
+if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise && uri) {
+        client = new MongoClient(uri);
+        global._mongoClientPromise = client.connect();
+    }
+    clientPromise = global._mongoClientPromise || Promise.reject(new Error('Missing MONGODB_URI environment variable.'));
+} else {
+    if (uri) {
+        client = new MongoClient(uri);
+        clientPromise = client.connect();
+    } else {
+        clientPromise = Promise.reject(new Error('Missing MONGODB_URI environment variable.'));
+    }
 }
 
 export default clientPromise;
